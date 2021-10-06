c0ac6b5a6b1d8e8684a6936c54ca76681ad38762
Juan Arias <juancamilo.arias@hotmail.com>
2021-10-06 13:31:15 -0400

    Modified elevator.cc

--- nachos-3.4/code/threads/elevator.cc
+++ nachos-3.4/code/threads/elevator.cc
@@ -28,7 +28,7 @@ struct PersonThread{
 struct ElevatorThread
 {
     int numFloors; 
-    int currFloor; 
+    int currentFloor; 
     int numPeopleIn; 
 };
 
@@ -37,67 +37,155 @@ int generateID(){
     id++; 
     return id; 
 }
+int count; //Track request fullfilled
 
 ElevatorThread eThread; 
-Thread *elevator; 
+bool movingUp = true;
 
+int *upQueue; //Analogous to pressing elevator button going up
+int *upToQueue; //Analogous to pressing desired floor inside when going up
+int *downQueue; //Analogous to pressing elevator button going down
+int *downToQueue; //Analogous to pressing desired floor inside when going down
 
-Semaphore *semaphore  = new Semaphore("Semaphore", 1); 
-Semaphore *barrier = new Semaphore("Barrier", 0);
+Semaphore *c = new Semaphore("Thread Count Semaphore", 1);
+Semaphore *q = new Semaphore("Queues", 1);
+Lock *e = new Lock("Elevator Lock");
+Condition *cv = new Condition("Elevator Condition");
 
+void ManagerHelper(int *inQueue, int *outQueue) {
+	q->P();
+	bool getIn = false;
+	bool getOut = false;
+	
+	int floor = eThread.currentFloor;
+	//Elevator checks if there are any people in the current floor that want to get in and can due to capacity
+	getIn = inQueue[floor] > 0 && eThread.numPeopleIn < 5;
+	//Elevator checks if there are any people in the current floor that want to get out
+	getOut = outQueue[floor] > 0;
+	
+	if (getIn || getOut) //Stop at current floor if anyone wants to get in or out
+		printf("Elevator arrives on floor %d.\n", eThread.currentFloor);
+	
+	e->Release();
+	q->V();
+	while (getIn || getOut) { //While there are people still wanting to get in or out
+		e->Acquire();
+		q->P();
+		getIn = inQueue[floor] > 0 && eThread.numPeopleIn < 5; //Check getin again
+		getOut = outQueue[floor] > 0; //Check getout again
+		cv->Broadcast(e); //Signal people to perform actions if any with the updated elevator information
+		q->V();
+		e->Release(); //Release lock so a person waiting can take the lock and process the elevator informaion
+		currentThread->Yield();
+	}
+	e->Acquire(); //Once no more people can perform any actions "close the doors" and begin moving
+	
+	for (int delay = 0; delay < 50; delay++); //Moving delay
+	
+	if (movingUp)
+		eThread.currentFloor++; //Move up one floor
+	else
+		eThread.currentFloor--; //Move down one floor
+}
 
-void ArrivingGoingFromToTest(int p){
-    if(eThread.numPeopleIn <= 5){// meet capacity requirements
-        printf("Too Many People in Elevator!"); 
-        return;
-    }
+void Manager (int which) {
+	currentThread->Yield();
+	e->Acquire();
+	while(true) { //Stay active until there are no more people in need of elevator
+		
+		if (eThread.currentFloor == 1) { //Elevator is on ground floor
+			movingUp = true; //Indicate its going to move up
+			ManagerHelper(upQueue, downToQueue); //People getting in are going up, People getting down were moving down
+		} else if (eThread.currentFloor == eThread.numFloors) { //Elevator is on top floor
+			movingUp = false; //Indicate its going to move down
+			ManagerHelper(downQueue, upToQueue); //People getting in are going down, People getting down were moving up
+		} else if (movingUp) { //Elevator is moving up
+			ManagerHelper(upQueue, upToQueue); //People getting in are going up, People getting down were moving up
+		} else if (!movingUp) { //Elevator is moving down
+			ManagerHelper(downQueue, downToQueue); //People getting in are going down, People getting down were moving down
+		}
+		
+		c->P();
+		if (id == count) //Check if all people that wanted to use the elevator (id) have arrived at their destination (count)
+			break; //Stop elevator
+		c->V();
+	}
+	e->Release();
+}
 
-    PersonThread *pThread = (PersonThread*) p; 
-    semaphore -> P(); //waits
-    printf("person %d  wants to go to floor %d from floor %d ", pThread-> id, pThread -> toFloor, pThread->atFloor);
-    
-    eThread.currFloor = pThread->atFloor; //elevator starts/arives at current floor
-    eThread.numPeopleIn++; //increase amount of people in elevator
-     
-    
-    printf("person %d got into the elevator", pThread -> id); 
-    currentThread -> Yield(); 
-
-	//floors are 50 clicks apart
-    for(int i = (pThread->atFloor * 50); i <= (pThread->toFloor * 50); i++){// loops until reaches destination 
-        semaphore -> V(); 
-        currentThread -> Yield(); 
-
-        if((i % 50) == 0){
-            eThread.currFloor++; 
-            printf("Elevator arrives at %d", eThread.currFloor); 
-        }
-    }
-
-    semaphore -> P(); 
-    printf("person %d got out of the elevator", pThread -> id);
-    eThread.numPeopleIn--; 
-
-	//use better solution to find out if remaining threads/people exist
-    if(eThread.numPeopleIn == 0){ 
-        barrier ->V(); 
-    }
-   
-    barrier ->P(); 
-    barrier ->V(); 
-
+void Person (int person) {
+	PersonThread *p = (PersonThread *) person;
+	bool goingUp = p->toFloor >= p->atFloor;
+	bool in = false;
+	
+	printf("Person %d wants to go to floor %d from floor %d.\n", p->id, p->toFloor, p->atFloor);
+	
+	q->P();
+	if (goingUp) 
+		upQueue[p->atFloor]++; //Person adds themselves to up queue (Press button to go up)
+	else
+		downQueue[p->atFloor]++; //Person add themselves to down queue (Press button to go down)
+	q->V();
+	
+	while(true) { //Stay active until person arrives at desired floor
+		e->Acquire();
+		cv->Wait(e); //Wait for elevator to be done moving and processing
+		bool readyEnter = eThread.currentFloor == p->atFloor; //Ready to enter the elevator
+		bool readyLeave = eThread.currentFloor == p->toFloor; //Ready to leave the elevator
+		bool capacity = eThread.numPeopleIn < 5; //Check if there is still space to go in
+		bool direction = (goingUp && movingUp) || (!goingUp && !movingUp); //Check if elevator is going the same way
+		if (p->atFloor == p->toFloor && readyEnter){ //Odd case where a persons current and desire floor are the same
+			q->P();
+			upQueue[p->atFloor]--; //Remove themselves from the waiting queue
+			q->V();
+			printf("Person %d got into the elevator.\n", p->id);
+			printf("Person %d got out of the elevator.\n", p->id);
+			break;
+		} else if (!in && readyEnter && direction && capacity) { //Person can go in the elevator
+			q->P();
+			if (goingUp) {
+				upToQueue[p->toFloor]++; //Person adds themselves to up destination queue (Press button of desired floor once inside)
+				upQueue[p->atFloor]--; //Remove person from waiting queue
+			}
+			else {
+				downToQueue[p->toFloor]++; //Person adds themselves to down destination queue (Press button of desired floor once inside)
+				downQueue[p->atFloor]--; //Remove person from waiting queue
+			}
+			q->V();
+			eThread.numPeopleIn++; //Person got into the elevator so increment count
+			in = true; //Person is in the elevator
+			printf("Person %d got into the elevator.\n", p->id);
+		} else if (in && readyLeave) { //Person in elevator arrived to desired floor
+			q->P();
+			if (goingUp)
+				upToQueue[p->toFloor]--; //Remove person from destination queue
+			else
+				downToQueue[p->toFloor]--; //Remove person from destination queue
+			q->V();
+			eThread.numPeopleIn--; //Person got out so decrement count
+			printf("Person %d got out of the elevator.\n", p->id);
+			break; //Leave while loop, person arrived at desired floor
+		}
+		e->Release(); //Release lock on elevator if person has no more available actions and is done using elevator information
+	}
+	c->P();
+	count++; //Person's request has been fulfilled so increment count
+	c->V();
+	e->Release(); //Release lock on elevator once person has arrived
 }
 
 void ArrivingGoingFromTo(int atFloor, int toFloor){
-    PersonThread *pThread;
+    PersonThread *pThread = new PersonThread;
     Thread *person; // create person thread
     person = new Thread("Creating Person Thread"); 
-    pThread -> id = generateID(); //generate id for person
     pThread -> atFloor = atFloor; //initialize current floor
     pThread -> toFloor = toFloor; //initialize desired floor 
-
-    person -> Fork(ArrivingGoingFromToTest, (int)pThread); 
-    ArrivingGoingFromToTest((int)pThread);// run simulation
+	
+	c->P(); //Shared variable id is also used to represent how many request are made
+	pThread -> id = generateID(); //generate id for person
+	c->V();
+	
+    person -> Fork(Person, (int)pThread); 
 }
 
 void Elevator(int numFloors){
@@ -106,11 +194,21 @@ void Elevator(int numFloors){
         return; 
     }
 
-    elevator = new Thread("Creating Elevator Thread");
+    Thread *elevator = new Thread("Creating Elevator Thread");
+	e->Acquire();
     eThread.numFloors = numFloors; //initialize num of floors for elevator thread
-    eThread.currFloor = 0; //start at ground floor
+    eThread.currentFloor = 1; //start at ground floor
     eThread.numPeopleIn = 0; //starts as empty
+	e->Release();
 
-    elevator -> Fork(Elevator, 1); 
+	q->P();
+	int size = numFloors+1; //Initialize waiting queues
+	upQueue = new int[size];
+	upToQueue = new int[size];
+	downQueue = new int[size];
+	downToQueue = new int[size];
+	q->V();
+
+    elevator -> Fork(Manager, 0); 
 
 }
